0bbfff5fc9ec170942d3fa71199feaf21d436f62
KenFilms <friedl@tum.de>
2022-01-28 10:40:18 +0100

    Update talker.py

--- example/talker.py
+++ example/talker.py
@@ -1,20 +1,19 @@
-   1 #!/usr/bin/env python
-   2 # license removed for brevity
-   3 import rospy
-   4 from std_msgs.msg import String
-   5 
-   6 def talker():
-   7     pub = rospy.Publisher('chatter', String, queue_size=10)
-   8     rospy.init_node('talker', anonymous=True)
-   9     rate = rospy.Rate(10) # 10hz
-  10     while not rospy.is_shutdown():
-  11         hello_str = "hello world %s" % rospy.get_time()
-  12         rospy.loginfo(hello_str)
-  13         pub.publish(hello_str)
-  14         rate.sleep()
-  15 
-  16 if __name__ == '__main__':
-  17     try:
-  18         talker()
-  19     except rospy.ROSInterruptException:
-  20         pass
+import rospy
+from std_msgs.msg import String
+   
+   
+def r2d2():
+   pub = rospy.Publisher('r2d2_hello', String, queue_size=10)
+   rospy.init_node('r2d2', anonymous=True)
+   rate = rospy.Rate(10) # 10h
+      while not rospy.is_shutdown():
+         r2d2_hello = "Hello world from R2." 
+         rospy.loginfo(r2d2_hello)
+         pub.publish(r2d2_hello)
+         rate.sleep()
+
+if __name__ == '__main__':
+   try:
+      r2d2()
+      except rospy.ROSInterruptException:
+         pass
